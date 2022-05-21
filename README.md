a3a5ca4e0fb2a4b6aaffb8abebd78b59fe0c6971
Gary J <github@accu.ca>
2022-05-20 18:34:46 -0700

    Merge

---
+++ README.md
@@ -0,0 +1,23 @@
+# React Native Navcgation
+
+### install packages
+
+https://reactnative.dev/docs/navigation
+
+```
+npm install react-native-screens react-native-safe-area-context
+npm install @react-navigation/native
+npx pod-install ios
+```
+
+### Edit `MainActivity.java` Java code for Android App's
+
+```
+import android.os.Bundle;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
+```
+https://reactnavigation.org/docs/getting-started
--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import Home from '/screen/Home';
 
 const App = function () {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar />
-      <Text style={styles.text}>Hello React Native</Text>
+    <SafeAreaView>
+      <Home />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {fontSize: 24, fontWeight: '500'},
-});
 
 export default App;
