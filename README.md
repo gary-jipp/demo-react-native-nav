bdd7e596775a1585b85d6a776f5937dfbb14a22a
Gary J <github@accu.ca>
2022-05-20 18:33:04 -0700

    Merge

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Native Navigation - Stack
+
+- new folder: src/navigator
+- create  `HomeStack` as component
+- Build Navigator inside HomeStack
+- Add a Navigator with Home & About Screens
+- Replace Navigator in App with HomeStack
+
+https://reactnavigation.org/docs/native-stack-navigator
--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeStack from '/navigator/HomeStack';
 
 const App = function () {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar />
-      <Text style={styles.text}>Hello React Native</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <HomeStack />
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {fontSize: 24, fontWeight: '500'},
-});
 
 export default App;
